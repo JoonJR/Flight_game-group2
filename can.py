5ad2a6c4cb63c529d557b7c8fce82b8abb60a4a0
canerman13 <cane@metropolia.fi>
2022-09-26 15:01:35 +0300

    can

---
+++ can.py
@@ -0,0 +1,14 @@
+nickname=input("Please enter your nickname")
+print(f"Hello {nickname}! Your task is to travel as many continents as possible! "
+      f"You have 4000 C02. For every 1000 km you use 100 CO2.`\n Your starting point is a random airport in"
+      f"a random country. Later you can chose any country, but the airport will be random."
+      f"\nBefore you fly, roll the dice. If you roll:"
+      f"\n6: no CO2 was used"
+      f"\n5: You use 50% of your CO2"
+      f"\n4: The plane had to come back and you wasted your CO2."
+      f"\n3: You end up in a random destination."
+      f"\n2: You use double the amount of your CO2"
+      f"\n1: Your plane crashes. The round is over."
+      f"\nYou have 3 rounds.")
+con= input("Press y/n to continue:")
+
