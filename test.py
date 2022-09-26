b00c49efc5fe8ba248ad7fa4cca50b89996e70e8
Nadim-2022 <i.am.nadim95@gmail.com>
2022-09-27 02:51:09 +0300

    test

--- test.py
+++ test.py
@@ -2,13 +2,26 @@ import mysql.connector
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
-         port= 3306,
+         port=3306,
          database='flight_game',
          user='root',
          password='root',
          autocommit=True
          )
 
+nickname=input("Please enter your nickname")
+print(f"Hello {nickname}! Your task is to travel as many continents as possible! "
+      f"You have 4000 C02. For every 1000 km you use 100 CO2.`\n Your starting point is a random airport in"
+      f"a random country. Later you can chose any country, but the airport will be random."
+      f"\nBefore you fly, roll the dice. If you roll:"
+      f"\n6: no CO2 was used"
+      f"\n5: You use 50% of your CO2"
+      f"\n4: The plane had to come back and you wasted your CO2."
+      f"\n3: You end up in a random destination."
+      f"\n2: You use double the amount of your CO2"
+      f"\n1: Your plane crashes. The round is over."
+      f"\nYou have 3 rounds.")
+con= input("Press y/n to continue:")
 def GetCountry():
     sql = "SELECT country.name FROM country ORDER BY RAND() LIMIT 1"
     cursor = connection.cursor()
@@ -54,6 +67,12 @@ def get_location (area_code):
         return location
 
 
-area_code1 = input("Enter area code of the first airport: ")
-area_code2 = input("Enter area code of the first airport: ")
-print(f"The distance between the two airports is {geodesic(get_location(area_code1), get_location(area_code2)).kilometers:.6} km")
+CO2_km = 0.1
+count = 0
+co2 = 0
+while count <3:
+    area_code1 = input("Enter area code of the first airport: ")
+    area_code2 = input("Enter area code of the second airport: ")
+    print(f"The distance between the two airports is {geodesic(get_location(area_code1), get_location(area_code2)).kilometers:.6} km"
+          f"\n and CO2 consumed {geodesic(get_location(area_code1), get_location(area_code2))*CO2_km} ")
+    count += 1
