d4148608301b7bf4d1c4ae01ae4ba1ecb5927126
karinka1901 <karindomagalska@gmail.com>
2022-09-26 14:30:04 +0300

    test

---
+++ test.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='root',
+         autocommit=True
+         )
+
+def GetCountry():
+    sql = "SELECT country.name FROM country ORDER BY RAND() LIMIT 1"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(row[0])
+        return
+
+GetCountry()
+
