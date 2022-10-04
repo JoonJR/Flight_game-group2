b093fb854f62846faac5f4cd2f16f470d397c497
JoonJR <joonajani.rantala@gmail.com>
2022-10-05 00:19:18 +0300

    Added cheat sheet for EU game.

--- Functions.py
+++ Functions.py
@@ -1,6 +1,15 @@
+import mysql.connector
 import time
-from time import sleep
 import sys
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port=3306,
+         database='flight_game',
+         user='root',
+         password='root',
+         autocommit=True
+         )
+
 
 def typewriter(rules):
     for char in rules:
@@ -11,3 +20,16 @@ def typewriter(rules):
             time.sleep(0.02)
         else:
             time.sleep(0.5)
+
+def cheat_sheet():
+    countrylist = [['Andorra  ', 'Albania  ', 'Austria  ', 'Belgium  ', 'Slovakia ', 'Bulgaria ', 'Belarus  ',
+                    'Serbia   ', 'Sweden   ', 'Germany  ', 'Denmark  ', 'Estonia  ', 'Spain    ', 'Finland  ',
+                    'Romania  '], ['France     ', 'Ukraine    ', 'Guernsey   ', 'Gibraltar  ', 'Greece     ', 'Croatia    ',
+                    'Hungary    ', 'Ireland    ', 'Monaco     ', 'Iceland    ', 'Italy      ', 'Jersey     ', 'Russia     ', 'Lithuania  ',
+                    'Luxembourg '], ['Latvia      ', 'Isle of Man ', 'Moldova     ', 'Montenegro  ', 'Kosovo      ', 'Malta  ',
+                    'Netherlands  ', 'Norway  ', 'Poland  ', 'Portugal  ', 'Faroe Islands  ', 'Switzerland  ', 'Liechtenstein  ', 'Czech Republic  ',
+                    'Slovenia  '],['Bosnia and Herzegovina  ', 'San Marino  ', 'United Kingdom  ', 'Vatican City  ', 'North Macedonia  ', '', '', '', '', '', '', '', '', '', '']]
+    print("CHEAT SHEET, MAKE SURE TEACHER DOESN'T SEE YOU!!!")
+    for x, y, z, q in zip(*countrylist):
+        print(x, y, z, q)
+
--- mainmenu.py
+++ mainmenu.py
@@ -1,26 +1,19 @@
-import mysql.connector
-import geopy
-from geopy.distance import geodesic
-import random
-import time
-from time import sleep
-import sys
 from continent_game_function import *
 from Functions import *
-connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port=3306,
-         database='flight_game',
-         user='root',
-         password='root',
-         autocommit=True
-         )
+
 print("Welcome to Green Wash Airlines")
-mainmenu = int(input("1. Continent game \n2. Europe game \n3. Quit \n"))
-if mainmenu == 1:
+main_menu = int(input("1. Continent game \n2. Europe game \n3. Quit \n"))
+if main_menu == 1:
     flight_game_continent()
-elif mainmenu == 2:
-    print("EU game")
-elif mainmenu == 3:
+elif main_menu == 2:
+    eu_menu = int(input("\nEU game\n1. Start \n2. Country cheat sheet. \n3. Quit \n"))
+    if eu_menu == 1:
+        print("function here")
+    elif eu_menu == 2:
+        print(cheat_sheet())
+    elif eu_menu == 3:
+        typewriter("lol")
+elif main_menu == 3:
     typewriter("Exiting the game.")
+
 
