e87705ba094d475979de582fa35fb36412743f5f
JoonJR <joonajani.rantala@gmail.com>
2022-10-05 01:07:48 +0300

    Added credits, easter egg :3

--- Functions.py
+++ Functions.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import time
 import sys
+import os
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port=3306,
@@ -19,17 +20,59 @@ def typewriter(rules):
         if char != "\n":
             time.sleep(0.02)
         else:
-            time.sleep(0.5)
+            time.sleep(1)
 
 def cheat_sheet():
     countrylist = [['Andorra  ', 'Albania  ', 'Austria  ', 'Belgium  ', 'Slovakia ', 'Bulgaria ', 'Belarus  ',
                     'Serbia   ', 'Sweden   ', 'Germany  ', 'Denmark  ', 'Estonia  ', 'Spain    ', 'Finland  ',
-                    'Romania  '], ['France     ', 'Ukraine    ', 'Guernsey   ', 'Gibraltar  ', 'Greece     ', 'Croatia    ',
-                    'Hungary    ', 'Ireland    ', 'Monaco     ', 'Iceland    ', 'Italy      ', 'Jersey     ', 'Russia     ', 'Lithuania  ',
-                    'Luxembourg '], ['Latvia      ', 'Isle of Man ', 'Moldova     ', 'Montenegro  ', 'Kosovo      ', 'Malta  ',
-                    'Netherlands  ', 'Norway  ', 'Poland  ', 'Portugal  ', 'Faroe Islands  ', 'Switzerland  ', 'Liechtenstein  ', 'Czech Republic  ',
-                    'Slovenia  '],['Bosnia and Herzegovina  ', 'San Marino  ', 'United Kingdom  ', 'Vatican City  ', 'North Macedonia  ', '', '', '', '', '', '', '', '', '', '']]
-    print("CHEAT SHEET, MAKE SURE TEACHER DOESN'T SEE YOU!!!")
+                    'Romania  '], ['France     ', 'Ukraine    ', 'Guernsey   ', 'Gibraltar  ', 'Greece     ',
+                    'Croatia    ', 'Hungary    ', 'Ireland    ', 'Monaco     ', 'Iceland    ', 'Italy      ',
+                    'Jersey     ', 'Russia     ', 'Lithuania  ', 'Luxembourg '], ['Latvia      ', 'Isle of Man ',
+                    'Moldova     ', 'Montenegro  ', 'Kosovo      ', 'Malta  ','Netherlands  ', 'Norway  ', 'Poland  ',
+                    'Portugal  ', 'Faroe Islands  ', 'Switzerland  ', 'Liechtenstein  ', 'Czech Republic  ',
+                    'Slovenia  '],['Bosnia and Herzegovina  ', 'San Marino  ', 'United Kingdom  ', 'Vatican City  ',
+                    'North Macedonia  ', '', '', '', '', '', '', '', '', '', '']]
+    print("\nCHEAT SHEET, MAKE SURE TEACHER DOESN'T SEE YOU!!!")
     for x, y, z, q in zip(*countrylist):
         print(x, y, z, q)
+
+def credits_text():
+    credits = "Designed and developed by Group 2\nKarin Domagalska\nJoona Rantala\nCan Erman\nNadim Ahmed"
+    typewriter(credits)
+    print("\n")
+
+
+def easter_egg():
+    while True:
+        egg = input("1. To exit.\nyes or no: ")
+        if egg == "yes":
+            egg = input("1. To exit.\nyes or no: ")
+            if egg == "yes":
+                egg = input("1. To exit.\nyes or no: ")
+                if egg == "yes":
+                    egg = input("1. To exit.\nyes or no: ")
+                    if egg == "yes":
+                        print(f"\n\n\n\U0001F49A\U0001F49APlease grade us 5/5\U0001F49A\U0001F49A\n\n\n")
+                        break
+                    elif egg == '1':
+                        print("\n")
+                        break
+                    elif egg == "no":
+                        egg = input("Are you sure? (yes/no): ")
+                elif egg == '1':
+                    print("\n")
+                    break
+                elif egg == "no":
+                    egg = input("Are you sure? (yes/no): ")
+            elif egg == '1':
+                print("\n")
+                break
+            elif egg == "no":
+                egg = input("Are you sure? (yes/no): ")
+        elif egg == '1':
+            print("\n")
+            break
+        elif egg == "no":
+            egg = input("Are you sure? (yes/no): ")
+
 
--- mainmenu.py
+++ mainmenu.py
@@ -1,19 +1,27 @@
 from continent_game_function import *
 from Functions import *
+import os
 
-print("Welcome to Green Wash Airlines")
-main_menu = int(input("1. Continent game \n2. Europe game \n3. Quit \n"))
-if main_menu == 1:
-    flight_game_continent()
-elif main_menu == 2:
-    eu_menu = int(input("\nEU game\n1. Start \n2. Country cheat sheet. \n3. Quit \n"))
-    if eu_menu == 1:
-        print("function here")
-    elif eu_menu == 2:
-        print(cheat_sheet())
-    elif eu_menu == 3:
-        typewriter("lol")
-elif main_menu == 3:
-    typewriter("Exiting the game.")
+while True:
+    print("Welcome to Green Wash Airlines")
+    main_menu = int(input("1. Continent game \n2. Europe game \n3. Credits \n4. Something funny. \n5. Quit\n"))
+    if main_menu == 1:
+        flight_game_continent()
+    elif main_menu == 2:
+        while True:
+            eu_menu = int(input("\nEU game\n1. Start \n2. Country cheat sheet. \n3. Back \n"))
+            if eu_menu == 1:
+                print("function here")
+            elif eu_menu == 2:
+                cheat_sheet()
+            elif eu_menu == 3:
+                break
+    elif main_menu == 3:
+        credits_text()
+    elif main_menu == 4:
+        easter_egg()
+    elif main_menu == 5:
+        typewriter("Exiting the game.")
+        break
 
 
