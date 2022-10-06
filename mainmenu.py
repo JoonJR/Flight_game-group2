eadcb2bf86a04be18ef9af009efaa77249363e86
karinka1901 <karindomagalska@gmail.com>
2022-10-06 22:14:56 +0300

    the game is done

--- mainmenu.py
+++ mainmenu.py
@@ -3,14 +3,17 @@ from Functions import *
 from ascii import *
 
 while True:
+    ascii_pictures(6)
     ascii_text(5)
-    typewriter("Welcome to Green Wash Airlines\n")
-    main_menu = int(input("1. Continent game \n2. Europe game \n3. Credits \n4. Something funny. \n5. Quit\n"))
+    ascii_pictures(6)
+    typewriter("\nWelcome to Sus-PLANE-ability\n")
+    main_menu = int(input("1. Continent game \n2. Europe game \n3. Credits \n4. Something funny. \n5. Quit\n>>> "))
     if main_menu == 1:
+        typewriter("Flying to the Continent Game. Please fasten your seatbelt...\n\n\n")
         flight_game_continent()
     elif main_menu == 2:
         while True:
-            eu_menu = int(input("\nEU game\n1. Start \n2. Country cheat sheet. \n3. Back \n"))
+            eu_menu = int(input("\nEU game\n1. Start \n2. Country cheat sheet. \n3. Back \n>>>"))
             if eu_menu == 1:
                 print("function here")
             elif eu_menu == 2:
