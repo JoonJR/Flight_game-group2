643daf0347ca1258cd179b36e08affc7b9ee1159
JoonJR <joonajani.rantala@gmail.com>
2022-10-11 13:39:22 +0300

    functions to separate files. Fixed few typos here and there.

--- Games/EU_game_function.py
+++ Games/EU_game_function.py
@@ -2,6 +2,7 @@ from geopy.distance import geodesic
 import random
 import os
 from Functions.Functions import *
+from Functions.EU_game_funtions import *
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port=3306,
@@ -12,87 +13,6 @@ connection = mysql.connector.connect(
          )
 
 def eugame():
-    def get_country():
-        sql = "SELECT country.name FROM  country WHERE continent = 'EU' order by RAND() LIMIT 1"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if cursor.rowcount > 0:
-            for row in result:
-                return row[0]
-
-
-    def get_heliport_code(country):
-        sql = "SELECT airport.ident FROM airport, country  WHERE country.continent = 'EU' and country.iso_country = airport.iso_country and country.name ='" + country +"' AND airport.type = 'heliport' OR country.continent = 'EU' and country.iso_country = airport.iso_country and country.name ='" + country +"' and airport.type = 'small_airport' OR country.continent = 'EU' and country.iso_country = airport.iso_country and country.name ='" + country +"' and airport.type = 'medium_airport' order by (case when airport.type = 'heliport' then 1 else 2 END) LIMIT 1"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if not result:
-            print("This country does not exists or is not located in continent EU, try again.")
-            return None
-
-        if cursor.rowcount > 0:
-            for row in result:
-                return row[0]
-
-    def get_heliport_name(code):
-        sql = "SELECT airport.name FROM airport WHERE airport.ident ='" + code + "'"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if cursor.rowcount > 0:
-            for row in result:
-                return row[0]
-
-
-    def get_location(code):  # returns long/lat of an airport
-        location = []
-        sql = "SELECT longitude_deg, latitude_deg FROM airport WHERE airport.ident ='" + code + "'"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if cursor.rowcount > 0:
-            for row in result:
-                location.append(int(row[1]))
-                location.append(int(row[0]))
-            return location
-
-
-    def typewriter(rules):
-        for char in rules:
-            sys.stdout.write(char)
-            sys.stdout.flush()
-
-            if char != "\n":
-                time.sleep(0.02)
-            else:
-                time.sleep(0.5)
-
-
-    death_text = ["Your helicopter got struck by lightning and crashed on a remote island, you survived the crash and promptly died of dehydration. Game Over!",
-                  "The pilot had one too many the night before and fell asleep at the wheel, nosediving the helicopter to your inevitable doom. Game Over!",
-                  "You encountered some unexpected turbulence causing the pilot to lose complete control of the helicopter which hits the ground and explodes, but not before doing a cool flip. Game Over!",
-                  "You poorly chose the meal with fish. You made it to your destination before dying of food poisoning in the airport bathroom. Game Over!",
-                  "Someone's beloved pet cobra somehow escaped from its cage and found its way up your pants. Unfortunately you only realised this after it bit you. You took your last breath 15 minutes later. Game Over!"]
-
-    neardeath_text = ["You tripped while leaving the helicopter and proceeded to fall down the stairs, you thought it was all over\n before the real Tom of Finland grabbed your arm, saving your life.",
-                      "Some strong turbulence managed to open a loose baggage compartment. Someones weights fell out and almost hit you on the head.\nThankfully they hit the person next to you.",
-                      "You were offered a meal choice of either fish or meat, you were about to choose fish before remembering the golden rule. That was a close one.",
-                      "You somehow managed to flush the toilet without realising a part of your clothing was in there.\nYou got pulled back into the toilet but thankfully you had your second breakfast that morning.",
-                      "Your helicopter was about to take off and then you quickly remembered you didnt change your phones mode to flight mode.\nYou frantically make the change before takeoff saving the lives of many. A true hero.",
-                      "A bump in the sky during mealtime caused a swedish meatball to go down the wrong pipe. Thankfully a vet was nearby to help."]
-
-    randomcountry_text = ["The pilot forgot to Never Eat Soggy Waffles and ended up going in the opposite direction.",
-                          "Your pilot partied a bit too much the night before and didn't realise a change of schedule.",
-                          "Your helicopter got hijacked by honeymooners who could not afford the flights for their dream trip",
-                          "You accidentally got onto the wrong helicopter without anyone noticing"]
-
-    fullrefund_text = ["You found an old coupon for a free flight on the floor of a public toilet. All expenses paid.",
-                       "Some rich guy ahead of you was feeling generous and paid for your flight. No Co2 spent.",
-                       "You forgot to buy the ticket and managed to sneak on the helicopter without getting caught. You even bumped yourself up to first class. No Co2 spent.",
-                       "You spoke to the pilot on your way onto the helicopter and it turns out hes your sisters cousins uncles brother. He let you on for free. No Co2 spent.",
-                       "You spoke to the pilot on your way onto the helicopter and it turns out hes your uncles sisters aunts nephew. He let you on for free. No Co2 spent."]
-
 
     player_name = input("\nEnter your name: ")
 
@@ -107,7 +27,7 @@ def eugame():
     2. You had to take an unexpected detour. Double the amount of Co2 consumed.\n\
     1. Worst possible scenario. You have a 50% chance of dying.\nGood luck!\nps if there are errors its your fault :p\n"
 
-    #typewriter(rules)
+    typewriter(rules)
     is_alive = True
     budget = 10000
     countries = []
--- Games/continent_game_function.py
+++ Games/continent_game_function.py
@@ -2,124 +2,15 @@ from geopy.distance import geodesic
 import random
 from Functions.Functions import *
 import os
-# import mysql.connector
-# import time
-# import sys
+from Functions.continent_game_functions import *
 # from ascii import *
-connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port=3306,
-         database='flight_game',
-         user='root',
-         password='root',
-         autocommit=True
-         )
 
 
 def flight_game_continent():
-    def get_country():
-        sql = "SELECT country.name FROM country, airport WHERE airport.iso_country = country.iso_country and airport.type like '%airport' ORDER BY RAND() LIMIT 1"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if cursor.rowcount > 0:
-            for row in result:
-                return row[0]
 
-    # def get_airport_code(country):
-    #     sql = "SELECT airport.ident FROM airport, country  WHERE country.iso_country = airport.iso_country and country.name ='" + country + "' AND airport.type = 'large_airport' OR country.iso_country = airport.iso_country and country.name ='" + country + "' and airport.type = 'medium_airport' OR country.iso_country = airport.iso_country and country.name ='" + country + "' and airport.type = 'small_airport' order by (case when airport.type = 'large_airport' then 1 ELSE 2 END) LIMIT 1"
-    #     cursor = connection.cursor()
-    #     cursor.execute(sql)
-    #     result = cursor.fetchall()
-    #     if not result:
-    #         print('This country does not exist, try again.')
-    #         return None
-    #
-    #     if cursor.rowcount > 0:
-    #         for row in result:
-    #             return row[0]
-    def get_airport_code(country):
-        sql = "SELECT airport.ident FROM airport, country  WHERE country.iso_country = airport.iso_country and country.name ='" + country + "' AND airport.type = 'medium_airport' OR country.iso_country = airport.iso_country and country.name ='" + country + "' and airport.type = 'large_airport' OR country.iso_country = airport.iso_country and country.name ='" + country + "' and airport.type = 'small_airport' order by (case when airport.type = 'medium_airport' then 1 ELSE 2 END), RAND() LIMIT 1"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if not result:
-            print('This country does not exist, try again.')
-            return None
-
-        if cursor.rowcount > 0:
-            for row in result:
-                return row[0]
-
-    def get_airport_name(code):
-        sql = "SELECT airport.name FROM airport WHERE airport.ident ='" + code + "'"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if cursor.rowcount > 0:
-            for row in result:
-                return row[0]
-
-    def get_continent(country):  # returns continent
-        sql = "SELECT country.continent FROM country WHERE country.name ='" + country + "'"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if cursor.rowcount > 0:
-            for row in result:
-                return row[0]
-
-    def get_location(code):  # returns long/lat of an airport
-        location = []
-        sql = "SELECT longitude_deg, latitude_deg FROM airport WHERE airport.ident ='" + code + "'"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        if cursor.rowcount > 0:
-            for row in result:
-                location.append(int(row[1]))
-                location.append(int(row[0]))
-            return location
-
-    def check_continents(current_continent):  # checks the list with the continents and removes it if the current continent is the same as in the list
+    def check_continents(current_continent):  # checks the list with the continents and appends it if the current continent is not in the list
         if current_continent not in continents:
             continents.append(current_continent)
-
-    def typewriter(rules):
-        for char in rules:
-            sys.stdout.write(char)
-            sys.stdout.flush()
-
-            if char != "\n":
-                time.sleep(0.02)
-            else:
-                time.sleep(0.0005)
-
-    death_text = [
-        "Your plane got struck by lightning and crashed on a remote island, you survived the crash and promptly died of dehydration.",
-        "The pilot had one too many the night before and fell asleep at the wheel, nosediving the plane to your inevitable doom.",
-        "You encountered some unexpected turbulence causing the pilot to lose complete control of the plane which hits the ground and explodes, but not before doing a cool flip.!",
-        "You poorly chose the meal with fish. You made it to your destination before dying of food poisoning in the airport bathroom.!",
-        "Someone's beloved pet cobra somehow escaped from its cage and found its way up your pants. Unfortunately you only realised this after it bit you. You took your last breath 15 minutes later."]
-
-    neardeath_text = [
-        "You tripped while leaving the plane and proceeded to fall down the stairs, you thought it was all over\n before the real Tom of Finland grabbed your arm, saving your life. Chills.",
-        "Some strong turbulence managed to open a loose baggage compartment. Someones weights fell out and almost hit you on the head.\nThankfully they hit the person next to you.",
-        "You were offered a meal choice of either fish or meat, you were about to choose fish before remembering the golden rule. That was a close one.",
-        "You somehow managed to flush the toilet without realising a part of your clothing was in there.\nYou got pulled back into the toilet but thankfully you had your second breakfast that morning.",
-        "Your plane was about to take off and then you quickly remembered you didnt change your phones mode to flight mode.\nYou frantically make the change before takeoff saving the lives of many. A true hero.",
-        "A bump in the sky during mealtime caused a swedish meatball to go down the wrong pipe. Thankfully a vet was nearby to help."]
-
-    randomcountry_text = ["The pilot forgot to Never Eat Soggy Waffles and ended up going in the opposite direction.",
-                          "Your pilot partied a bit too much the night before and didn't realise a change of schedule.",
-                          "Your plane got hijacked by honeymooners who could not afford the flights for their dream trip",
-                          "You accidentally got onto the wrong plane without anyone noticing"]
-
-    fullrefund_text = ["You found an old coupon for a free flight on the floor of a public toilet. All expenses paid.",
-                       "Some rich guy ahead of you was feeling generous and paid for your flight. No Co2 spent.",
-                       "You forgot to buy the ticket and managed to sneak on the plane without getting caught. You even bumped yourself up to first class. No Co2 spent.",
-                       "You spoke to the pilot on your way onto the plane and it turns out hes your sisters cousins uncles brother. He let you on for free. No Co2 spent.",
-                       "You spoke to the pilot on your way onto the plane and it turns out hes your uncles sisters aunts nephew. He let you on for free. No Co2 spent."]
 
     #ascii_pictures(5)
     ascii_text(3)
@@ -215,7 +106,7 @@ Every time before you fly a dice of destiny will be rolled. The outcomes of the 
                             typewriter(neardeath_text[random.randint(0, 5)])
                             typewriter(f"\nYour flight was {distance:.1f} kilometers and you had to pay {(distance / 10) * 2:.1f} Co2")
 
-                        if number == 3:  # 3. Your planes GPS breaks and you end up at a random destination anywhere in the world.\n\
+                        if number == 3:  # 3. Your planes GPS breaks, and you end up at a random destination anywhere in the world.\n\
 
                             random_country = get_country()  # the new random country
                             new_destination = get_airport_code(random_country)  # the location of the airport in a new random country
@@ -294,7 +185,7 @@ Every time before you fly a dice of destiny will be rolled. The outcomes of the 
                             typewriter("Your plane had to take an unexpected detour, doubling the cost of Co2.")
                             typewriter(f"\nYour flight was {distance:.1f} kilometers and you had to pay {(distance / 10) * 2:.1f} Co2\n")
 
-                        if number == 3:  # 3. Your planes GPS breaks and you end up at a random destination anywhere in the world.\n\
+                        if number == 3:  # 3. Your planes GPS breaks, and you end up at a random destination anywhere in the world.\n\
 
                             random_country = get_country()  # the new random country
                             new_destination = get_airport_code(random_country)  # the location of the airport in a new random country
@@ -308,7 +199,7 @@ Every time before you fly a dice of destiny will be rolled. The outcomes of the 
                         if number == 4:  # 4. Your plane had to return to the previous airport. Full amount of Co2 wasted
 
                             recent_airport += current_airport
-                            distance = geodesic(get_location(current_airport),get_location(destination_airport)).kilometers
+                            distance = geodesic(get_location(current_airport), get_location(destination_airport)).kilometers
                             budget -= int(distance / 10)
                             typewriter("Your plane had to return to the previous airport. Full amount of Co2 had to be paid.")
                             typewriter(f"\nYour flight was {0} kilometers and you had to pay {(distance / 10):.1f} Co2\n")
@@ -357,10 +248,8 @@ Every time before you fly a dice of destiny will be rolled. The outcomes of the 
         if decision == "N" or decision == 'n':
             typewriter("Flying back to the main menu. Please fasten your seatbelt...")
             time.sleep(1)
-            os.system('cls') # opens main menu in a new page (clears everything before that) but doesnt work in pycharm only if its open as a .py file
+            os.system('cls')  # opens main menu in a new page (clears everything before that) but doesn't work in pycharm only if its open as a .py file
             logo()
             return None
         else:
             typewriter("Invalid input!")
-
-# flight_game_continent()
